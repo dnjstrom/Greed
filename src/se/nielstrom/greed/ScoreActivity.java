8de0ec6ba3e2ddde2af612eca4ace4c94fc59bf0
Daniel Ström <D@nielstrom.se>
2014-06-22 18:38:29 +0200

    Unify the colors and make sure all buttons represent state properly.

---
+++ src/se/nielstrom/greed/ScoreActivity.java
@@ -0,0 +1,11 @@
+package se.nielstrom.greed;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScoreActivity extends Activity {
+	@Override
+    protected void onCreate(Bundle state) {
+		
+	}
+}
