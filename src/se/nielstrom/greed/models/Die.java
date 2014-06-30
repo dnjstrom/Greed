b411c672de8c59cb89068f2e66e1a23d8412954b
Daniel Ström <D@nielstrom.se>
2014-06-30 12:55:18 +0200

    Did a completely separated game model.

--- src/se/nielstrom/greed/models/Die.java
+++ src/se/nielstrom/greed/models/Die.java
@@ -1,38 +1,23 @@
 package se.nielstrom.greed.models;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class Die {
-	private static final int DEFAULT_NR_SIDES = 6;
-	public final int NR_OF_SIDES;
-	
-	private final List<DieChangeListener> changeListeners;
-	
+	private final int NR_OF_SIDES;
 	private int value;
 	private boolean locked;
-	
+
 	public Die() {
-		this(DEFAULT_NR_SIDES);
+		this(6);
 	}
-	
+
 	public Die(int nrOfSides) {
-		this(nrOfSides, nrOfSides);
+		NR_OF_SIDES = nrOfSides;
+		setValue(NR_OF_SIDES);
 	}
-	
-	public Die(int nrOfSides, int value) {
-		changeListeners = new ArrayList<>();
-		this.NR_OF_SIDES = nrOfSides;
-		
-		setValue(value);
-		setLocked(false);
-	}
-	
-	public void roll() {
-		if (!isLocked()) {
-			setValue((new Random()).nextInt(NR_OF_SIDES) + 1);
-		}
+
+	public Die roll() {
+		return setValue( (new Random()).nextInt(NR_OF_SIDES) + 1 );
 	}
 
 	public int getValue() {
@@ -40,12 +25,15 @@ public class Die {
 	}
 
 	public Die setValue(int value) {
-		this.value = value;
-		
-		for(DieChangeListener listener : changeListeners) {
-			listener.onDieChanged(this);
+		if (isLocked()) {
+			return this;
+		} else if (value < 1 || NR_OF_SIDES < value) {
+			throw new IllegalArgumentException("Can only set a number between 1 and "
+				+ NR_OF_SIDES
+				+ " - " + value + " attempted.");
 		}
-		
+
+		this.value = value;
 		return this;
 	}
 
@@ -53,20 +41,37 @@ public class Die {
 		return locked;
 	}
 
-	public void setLocked(boolean locked) {
+	public Die setLocked(boolean locked) {
 		this.locked = locked;
+		return this;
 	}
 
-	public void addChangeListener(DieChangeListener listener) {
-		changeListeners.add(listener);
-		listener.onDieChanged(this);
-	}
-	
-	public void removeChangeListener(DieChangeListener listener) {
-		changeListeners.remove(listener);
-	}
-	
-	public interface DieChangeListener {
-		public void onDieChanged(Die die);
+	public static void main(String[] args) {
+		Die die = new Die(6);
+		System.out.println("" + die.getValue());
+
+		System.out.println("-----");
+
+		System.out.println("" + die.setValue(1).getValue());
+		System.out.println("" + die.setValue(3).getValue());
+		System.out.println("" + die.setValue(6).getValue());
+
+		System.out.println("-----");
+
+		for (int i=1; i<=100; i++) {
+			System.out.print(die.roll().getValue());
+			if (i % 20 == 0) {
+				System.out.println("");
+			}
+		}
+
+		System.out.println("-----");
+
+		die.setValue(6);
+		die.setLocked(true);
+		for (int i=1; i<=10; i++) {
+			System.out.print(die.roll().getValue());
+		}
+
 	}
 }
--- src/se/nielstrom/greed/models/Greed.java
+++ src/se/nielstrom/greed/models/Greed.java
@@ -1,78 +1,147 @@
-package se.nielstrom.greed.models;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
 
-import se.nielstrom.greed.views.DieButton;
-
 public class Greed {
-	public static final int MIN_SCORE = 300;
-	public final int NR_OF_DICE;
-	private int totalScore;
-	private int roundScore;
-	private int rounds;
-	private Die[] dice;
-	
-	public Greed(int nrOfDice) {
-		roundScore = 0;
-		totalScore = 0;
-		rounds = 0;
-		NR_OF_DICE = nrOfDice;
-		
-		dice = new Die[NR_OF_DICE];
-		for(int i=0; i<NR_OF_DICE; i++) {
-			dice[i] = new Die();
+	private static final int NR_OF_DICE = 6;
+	private static final int NR_OF_SIDES = 6;
+	private static final int MIN_SCORE = 300;
+
+	private final Die[] dice;
+	private int scoreTotal;
+	private int scoreRound;
+	private int scoreRoundBonus;
+	private int round;
+
+	public Greed() {
+		dice = new Die[6];
+		for (int i=0; i<NR_OF_DICE; i++) {
+			dice[i] = new Die(NR_OF_SIDES);
 		}
+
+		scoreTotal = 0;
+		scoreRound = 0;
+		scoreRoundBonus = 0;
+		round = 0;
 	}
-	
-	public boolean roll() {
-		if (allDiceAreLocked() && allDiceAreUsed(dice)) {
-			
-			unlockDice();
+
+	public Greed roll() {
+		if (allDiceAreLocked() && allDiceAreUsed()) {
+			scoreRoundBonus += getScoreRound();
+			setAllDiceLocked(false);
 		}
+
 		rollDice();
-		
-		int turnScore = calculateScore(dice);
-		
-		if ((turnScore+roundScore) > Math.max(roundScore, MIN_SCORE-1)) {
-			roundScore += turnScore;
-			return true;
-		} else {
-			roundScore = 0;
-			return false;
-		}		
+
+		int score = calculateScore() + scoreRoundBonus;
+
+		// Low score or bust
+		if ( score <= Math.max(MIN_SCORE-1, getScoreRound()) ) {
+
+		}
+
+		setScoreRound(score);
+
+		return this;
 	}
-	
-	public void claim() {
-		totalScore += roundScore;
-		rounds++;
+
+	public int getScoreRound() {
+		return scoreRound;
 	}
-	
+
+	public Greed setScoreRound(int roundScore) {
+		this.scoreRound = roundScore;
+		return this;
+	}
+
+	public int getScoreTotal() {
+		return scoreTotal;
+	}
+
+	public Greed setScoreTotal(int totalScore) {
+		this.scoreTotal = totalScore;
+		return this;
+	}
+
+	public Greed appendScoreTotal(int score) {
+		setScoreTotal(getScoreTotal() + score);
+		return this;
+	}
+
+	public Greed claim() {
+		appendScoreTotal(getScoreRound());
+		setScoreRound(0);
+		setAllDiceLocked(false);
+		setRound( getRound() + 1 );
+		return this;
+	}
+
+	public Greed rollDice() {
+		for (Die die : dice) {
+			die.roll();
+		}
+		return this;
+	}
+
+	public Greed lockDie(int i) {
+		dice[i].setLocked(true);
+		return this;
+	}
+
+	public Greed setAllDiceLocked(boolean locked) {
+		for (Die die : dice) {
+			die.setLocked(locked);
+		}
+		return this;
+	}
+
 	public boolean allDiceAreLocked() {
-		for(Die die : dice) {
+		for (Die die : dice) {
 			if (!die.isLocked()) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
-	private void unlockDice() {
-		for(Die die: dice) {
-			die.setLocked(false);
-		}
-	}
-	
-	private void rollDice() {
-		for(Die die: dice) {
-			die.roll();
-		}
+
+	public int getRound() {
+		return round;
 	}
 
+	public Greed setRound(int round) {
+		this.round = round;
+		return this;
+	}
+
+	public Greed reset() {
+		setScoreTotal(0);
+		setScoreRound(0);
+		scoreRoundBonus = 0;
+		setAllDiceLocked(false);
+		return this;
+	}
+
+	/**
+	 * Convenience version of {@link #allDiceAreUsed(Integer... sides) allDiceAreUsed}
+	 * which uses the instance dice automatically.
+	 */
+	private boolean allDiceAreUsed() {
+		List<Integer> sides = new ArrayList<>();
+		for (Die die : dice) {
+			sides.add(die.getValue());
+		}
+		return allDiceAreUsed(sides);
+	}
+
+	/**
+	 * Overloads {@link #allDiceAreUsed(Integer... sides) allDiceAreUsed} to
+	 * work with any Collection<Integer>.
+	 * 
+	 * @param dice An Integer collection of sides (ranging from 1 through 6)
+	 */
+	private boolean allDiceAreUsed(Collection<Integer> dice) {
+		Integer[] diceArray = dice.toArray(new Integer[dice.size()]);
+		return allDiceAreUsed(diceArray);
+	}
 
 	/**
 	 * @param dice	A parameter list or array of Integers representing the side
@@ -80,12 +149,12 @@ public class Greed {
 	 * @return		True if all dice contribute points to the total score, False
 	 * 				otherwise
 	 */
-	public boolean allDiceAreUsed(Die... dice) {
+	private boolean allDiceAreUsed(Integer... sides) {
 		// Count the number of dice of each side
-		Map<Integer, Integer> sideAmount = aggregateDice(dice);
+		Map<Integer, Integer> diceMap = aggregateDice(sides);
 
-		if (sideAmount.size() != NR_OF_DICE) { // unless we have a ladder
-			for (Entry<Integer, Integer> entry : sideAmount.entrySet()) { // for each kind of die
+		if (diceMap.size() != NR_OF_DICE) { // unless we have a ladder
+			for (Entry<Integer, Integer> entry : diceMap.entrySet()) { // for each kind of die
 				if ((entry.getKey() != 1 && entry.getKey() != 5) // if the side is 2,3,4 or 6
 						&& entry.getValue() % 3 != 0) { // and the number of dice is not a multiple of 3
 					return false;
@@ -96,6 +165,30 @@ public class Greed {
 		return true;
 	}
 
+
+	/**
+	 * Convenience version of {@link #aggregateDice(Integer... dice) aggregateDice}
+	 * which uses the instance dice automatically.
+	 */
+	private Map<Integer, Integer> aggregateDice() {
+		List<Integer> sides = new ArrayList<>();
+		for (Die die : dice) {
+			sides.add(die.getValue());
+		}
+		return aggregateDice(sides);
+	}
+
+	/**
+	 * Overloads {@link #aggregateDice(Integer... dice) aggregateDice} to
+	 * work with any Collection<Integer>.
+	 * 
+	 * @param dice An Integer collection of sides (ranging from 1 through 6)
+	 */
+	private Map<Integer, Integer> aggregateDice(Collection<Integer> dice) {
+		Integer[] diceArray = dice.toArray(new Integer[dice.size()]);
+		return aggregateDice(diceArray);
+	}
+
 	/**
 	 * Counts the number of dice showing a given side.
 	 * 
@@ -103,16 +196,40 @@ public class Greed {
 	 * 				of a dice (valid sides: 1-6).
 	 * @return		A map of (side, number of dice with this side) pairs. 
 	 */
-	private Map<Integer, Integer> aggregateDice(Die... dice) {
-		Map<Integer, Integer> sideAmount = new HashMap<>(dice.length);
+	private Map<Integer, Integer> aggregateDice(Integer... dice) {
+		Map<Integer, Integer> diceMap = new HashMap<>(dice.length);
 
-		for (Die die : dice) {
-			Integer previousNumber = sideAmount.get(die.getValue());
+		for (Integer side : dice) {
+			Integer previousNumber = diceMap.get(side);
 			int newNumber = (previousNumber == null) ? 1 : previousNumber + 1;
-			sideAmount.put(die.getValue(), newNumber);
+			diceMap.put(side, newNumber);
 		}
 
-		return sideAmount;
+		return diceMap;
+	}
+
+
+	/**
+	 * Convenience version of {@link #calculateScore(Integer... dice) calculateScore}
+	 * which uses the instance dice automatically.
+	 */
+	private int calculateScore() {
+		List<Integer> sides = new ArrayList<>();
+		for (Die die : dice) {
+			sides.add(die.getValue());
+		}
+		return calculateScore(sides);
+	}
+
+	/**
+	 * Overloads {@link #calculateScore(Integer... dice) calculateScore} to
+	 * work with any Collection<Integer>.
+	 * 
+	 * @param dice An Integer collection of sides (ranging from 1 through 6)
+	 */
+	private int calculateScore(Collection<Integer> dice) {
+		Integer[] diceArray = dice.toArray(new Integer[dice.size()]);
+		return calculateScore(diceArray);
 	}
 
 	/**
@@ -122,17 +239,17 @@ public class Greed {
 	 * 				of a dice (valid sides: 1-6).
 	 * @return		The total score for all dice.
 	 */
-	public int calculateScore(Die... dice) {
+	private int calculateScore(Integer... dice) {
 		int score = 0;
 
 		// Count the dice by value
-		Map<Integer, Integer> sideAmount = aggregateDice(dice);
+		Map<Integer, Integer> diceMap = aggregateDice(dice);
 
-		if (sideAmount.size() == NR_OF_DICE) { // 6 different sides means it's a ladder
+		if (diceMap.size() == NR_OF_DICE) { // 6 different sides means it's a ladder
 			score = 1000;
 		} else {
 			// Calculate the score for each kind of side
-			for (Entry<Integer, Integer> entry : sideAmount.entrySet()) {
+			for (Entry<Integer, Integer> entry : diceMap.entrySet()) {
 				score += scoreHelper(entry.getKey(), entry.getValue());
 			}
 		}
@@ -144,20 +261,102 @@ public class Greed {
 	 * Recursive method which returns the score for a number of dice showing
 	 * the same side.
 	 * 
-	 * @param value		The side shown by each dice.
+	 * @param side		The side shown by each dice.
 	 * @param number	The number of dice showing said side.
 	 * @return			The calculated score
 	 */
-	private int scoreHelper(int value, int number) {
+	private int scoreHelper(int side, int number) {
 		if (number >= 3) {
-			int score = (value == 1) ? 1000 : 100 * value;
-			return score + scoreHelper(value, number - 3);
-		} else if (value == 1) {
+			int score = (side == 1) ? 1000 : 100 * side;
+			return score + scoreHelper(side, number - 3);
+		} else if (side == 1) {
 			return number * 100;
-		} else if (value == 5) {
+		} else if (side == 5) {
 			return number * 50;
 		} else {
 			return 0;
 		}
 	}
+
+
+	////////////////////////////////////////
+	/////          Debugging           /////
+	/////   (poor mans Unit-testing)   /////
+	////////////////////////////////////////
+
+	public static void main(String[] args) {
+		Greed game = new Greed();
+		game.printDiceLn();
+
+		game.printHeadline("Rolling dice");
+
+		for (int i=0; i<5; i++) {
+			game.rollDice().printDice();
+			System.out.println(": " + game.calculateScore());
+		}
+
+		game.printHeadline("Overflow turn")
+			.reset()
+			.setDice(1,2,3,4,5,6)
+			.setAllDiceLocked(true)
+			.setScoreRound(game.calculateScore())
+			.printState();
+
+		game.roll().printState();
+
+		game.printHeadline("Claiming a turn")
+			.reset()
+			.setDice(1,2,3,4,5,6)
+			.setAllDiceLocked(true)
+			.setScoreRound(game.calculateScore())
+			.printState();
+
+		game.claim().printState();
+
+	}
+
+	private Greed printState() {
+		printDice();
+		System.out.println(" ("
+			+ getRound()
+			+ "): "
+			+ getScoreRound()
+			+ ", "
+			+ getScoreTotal()
+		);
+		return this;
+	}
+
+	private Greed setDice(Integer... values) {
+		for (int i=0; i<dice.length && i<values.length; i++) {
+			dice[i].setValue(values[i]);
+		}
+		return this;
+	}
+
+	private Greed printDice() {
+		for (Die die : dice) {
+			System.out.print("" + die.getValue());
+		}
+		return this;
+	}
+
+	private Greed printDiceLn() {
+		printDice();
+		System.out.println("");
+		return this;
+	}
+
+	private Greed printHeadline(String headline) {
+		System.out.println("");
+		System.out.println(headline);
+
+		for (char c : headline.toCharArray()) {
+			System.out.print("-");
+		}
+
+		System.out.println("");
+		return this;
+	}
+
 }
